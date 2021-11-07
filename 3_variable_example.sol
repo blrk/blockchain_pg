aff08cb5819f662dee572c23640fc2ab1038e308
blrk <blradhakrishnan@gmail.com>
2021-11-07 21:35:52 +0530

    Update 3_variable_example.sol

--- 3_variable_example.sol
+++ 3_variable_example.sol
@@ -1,1 +1,9 @@
-progama
+pragma solidity ^0.8.3;
+
+contract variableExamples {
+  bool switchedOn = true;
+  address owner = msg.sender;
+  unit10 number = 10;
+  byte32 info = "Solidity is Great!";
+  string info1 = "Solidity is Great!";
+}
