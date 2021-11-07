b3584af46294546fc9b4f4aa36104b2f12235750
blrk <blradhakrishnan@gmail.com>
2021-11-07 21:37:03 +0530

    Create 3.1_variable_example.sol

---
+++ 3.1_variable_example.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.3;
+
+contract variableExamples {
+  bool public switchedOn = true;
+  address public owner = msg.sender;
+  unit10 public number = 10;
+  byte32 public info = "Solidity is Great!";
+  string public info1 = "Solidity is Great!";
+}
