5664a3963e810736050d72303aa9cf46660ff65b
blrk <blradhakrishnan@gmail.com>
2021-11-09 12:41:32 +0530

    Update 1.1_hello_world_modified.sol

--- 1.1_hello_world_modified.sol
+++ 1.1_hello_world_modified.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // compiler version must be greater than or equal to 0.8.3 and less than 0.9.0
-pragma solidity ^0.8.3;
+pragma solidity >=0.4.22 <0.9.0;
 
 contract HelloWorldM {
     function get()public pure returns (string memory){
