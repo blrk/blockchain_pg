59041866bd9e9d51e00a74bf754ebe289007268b
blrk <blradhakrishnan@gmail.com>
2021-11-15 08:59:22 +0530

    Update 3.1_variable_example.sol

--- 3.1_variable_example.sol
+++ 3.1_variable_example.sol
@@ -1,9 +1,10 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
 contract variableExamples {
   bool public switchedOn = true;
   address public owner = msg.sender;
-  unit10 public number = 10;
-  byte32 public info = "Solidity is Great!";
+  uint256 public number = 10;
+  bytes32 public info = "Solidity is Great!";
   string public info1 = "Solidity is Great!";
 }
