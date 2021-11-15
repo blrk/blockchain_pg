7b35e7094005e723ce0708925210321e380493ac
blrk <blradhakrishnan@gmail.com>
2021-11-15 09:51:46 +0530

    Update 4_getter_setter.sol

--- 4_getter_setter.sol
+++ 4_getter_setter.sol
@@ -5,7 +5,7 @@ contract SampleContract{
   string private msg = "Hello cyber security PG";
   
   // this function can't modify the msg because of constant
-  function getMessage() poublic constant returns(string){
+  function getMessage() public constant returns(string){
     return msg;
   }
   function setMessage(string smsg) public {
